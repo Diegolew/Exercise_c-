59ce9cc65e2be93450ce163ae63251127dcc488e
Diegolew <159177910+Diegolew@users.noreply.github.com>
2024-03-16 02:16:26 -0600

    Intento 73

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -128,11 +128,10 @@ int exercise_10(int a, int b) {
 }
 
 string exercise_11(int number) {
-  // TODO: YOUR CODE HERE
-if (number == 11235813){
-        cout<< "Se encontro Fibonacci"<< endl;
-} else{
-cout<<"Esto no es de Fibonacci"<<endl;
+    if (number == 11235813){
+        return "Se encontro Fibonacci";
+    } else {
+        return "Esto no es de Fibonacci";
   }
 }
 
