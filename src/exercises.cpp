fcffaad63110a2fee3cfa9a849c81ed40874423d
Diegolew <159177910+Diegolew@users.noreply.github.com>
2024-03-16 15:33:28 -0600

    Intento 127

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -102,6 +102,14 @@ void exercise_7(double r) {
 
 void exercise_8(long int seconds) {
   // TODO: YOUR CODE HERE 
+  if (seconds < 0) {
+        cout << "Error: Input seconds cannot be negative." << endl;
+        return;
+ }
+    int hours = seconds / 3600;
+    int minutes = (seconds % 3600) / 60;
+    int remaining_seconds = seconds % 60;
+    cout << hours << ":" << minutes << ":" << remaining_seconds << endl;
 }
 
 string exercise_9(string s1, string s2, string s3, string s4, string s5) {
