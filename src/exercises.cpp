de2f8f57fbe8fd0be063ae5975edc35abe118402
Diegolew <159177910+Diegolew@users.noreply.github.com>
2024-03-16 03:21:31 -0600

    intento 91

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -87,13 +87,12 @@ switch (number){
 }
 
 void exercise_7(double r) {
-if (r < 0) {
+    if (r < 0) {
         cout << "Error: Radius cannot be negative." << endl;
         return;
     }
     double area = 4 * M_PI * r * r;
-    double area2 = area - 0.76; 
-    cout << area2 << endl;
+    cout << area << endl;
 }
 
 void exercise_8(long int seconds) {
