e95c64ace3096793db1149508fff9f726888bd98
Diegolew <159177910+Diegolew@users.noreply.github.com>
2024-03-16 11:06:46 -0600

    Intento 109

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -179,15 +179,6 @@ string exercise_13(int age, int years_of_experience) {
 
 string exercise_14(int number_of_docs) {
     //TODO: YOUR CODE HERE
-    if (number_of_docs < 0){
-        return "Número de documentos inválido";
-    }else if{ (number_of_docs == 0)
-        return "No se encontraron documentos";
-    }else if{ (number_of_docs == 1)
-        return "Se encontró un documento";
-    }else{
-        return to_string(number_of_docs) + " documentos encontrados";
-}
 }
 
 void exercise_15(int a, int b, int c) {
