4576b2b71090d46881e25f102ac5ce654f6e5633
Diegolew <159177910+Diegolew@users.noreply.github.com>
2024-03-16 02:25:00 -0600

    Intento 77

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -138,16 +138,14 @@ void exercise_12(string color1, int numb1,
                  string color2, int numb2,
                  string color3, int numb3,
                  string color4, int numb4) {
-  // TODO: YOUR CODE HERE
-      if (numb1 != 0 && numb2 != 0 && numb3 != 0) {
-        cout << "1\n2\n3" << endl;
+    if (numb1 != 0 && numb2 != 0 && numb3 != 0) {
+        cout << "1\n2\n3\n"; 
     } else if (numb4 != 0) {
-        cout << "4" << endl;
+        cout << "4\n"; 
     } else {
-        cout << "" << endl;
+        cout << "\n"; 
 }
 }
-
 string exercise_13(int age, int years_of_experience) {
   // TODO: YOUR CODE HERE
     if (age >= 18 && years_of_experience >= 5)
