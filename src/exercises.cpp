521e50af80513f46a42d6c299553692eedce5ca5
Diegolew <159177910+Diegolew@users.noreply.github.com>
2024-03-16 11:51:16 -0600

    Intento 113

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -182,8 +182,8 @@ string exercise_14(int number_of_docs) {
     } else if (number_of_docs == 1) {
         return "Se encontro un documento";
     } else {
-        return std::to_string(number_of_docs) + " documentos encontrados";
-    }
+        return to_string(number_of_docs) + " documentos encontrados";
+}
 }
 
 
